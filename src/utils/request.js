50df99cb3ec0f6e350626e6203cfd640223cdcf8
yeyunwen <1571374338@qq.com>
2023-10-21 17:10:36 +0800

    feat: axios响应拦截器添加错误提醒

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,15 +1,25 @@
 import axios from "axios";
+import { ElMessage } from "element-plus";
 
 const service = axios.create({
-  baseURL: "/dev/api",
+  baseURL: "/api", // 存在硬编码 强耦合
   timeout: 50000,
 });
 
-service.interceptors.response.use((response) => {
-  const { code } = response.data;
-  if (code === 200) {
-    return response.data;
+service.interceptors.response.use(
+  (response) => {
+    const { code, msg } = response.data;
+    if (code === 200) {
+      return response.data;
+    }
+    ElMessage.error(msg || "系统出错");
+  },
+  (error) => {
+    console.log("err", error);
+    ElMessage.error(error);
+
+    return Promise.reject(error);
   }
-});
+);
 
 export default service;
