fb9dab707002c31a2352a6114417b9ebddf1faa0
yeyunwen <1571374338@qq.com>
2023-10-22 19:01:03 +0800

    feat: axios 完善 拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,26 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
+import { useUserStoreHook } from "@/store/modules/user";
 
 const service = axios.create({
   baseURL: "/api", // 存在硬编码 强耦合
   timeout: 50000,
 });
+
+service.interceptors.request.use(
+  (config) => {
+    const userStore = useUserStoreHook();
+    if (userStore.token) {
+      config.headers.Authorization = userStore.token;
+    }
+    console.log(userStore.token);
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 service.interceptors.response.use(
   (response) => {
