2ba545ab015489bd1714ac057574e91c019e4329
yeyunwen <1571374338@qq.com>
2023-10-26 22:37:17 +0800

    feat: 添加elemet-plus 和 icon 自动导入，但是icon好像没用效果

--- src/main.js
+++ src/main.js
@@ -1,20 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-import ElementPlus from "element-plus";
 import { setUpStore } from "@/store";
 
 import "normalize.css";
-import "element-plus/dist/index.css";
 import "@/styles/index.scss";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
-}
-
 setUpStore(app);
 
-app.use(ElementPlus).use(router).mount("#app");
+app.use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,7 +16,34 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: "Icon",
+          }),
+        ],
+        dts: false,
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"], // @iconify-json/ep 是 Element Plus 的图标库
+          }),
+        ],
+        dts: false,
+      }),
+      Icons({
+        autoInstall: true,
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
