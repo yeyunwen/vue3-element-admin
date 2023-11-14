69d5e81c36e077c23b5250d0cd6ebb46408824dd
yeyunwen <1571374338@qq.com>
2023-11-14 23:25:50 +0800

    feat: :sparkles: 添加登录跳转逻辑和表单校验

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,29 +1,71 @@
 <script setup lang="ts">
 import { UserFilled, Lock } from "@element-plus/icons-vue";
+import router from "@/router";
+
+// 状态管理
 import { useUserStore } from "@/store/modules/user";
+
+// api
 import { LoginData } from "@/api/user/types";
 
-const userStore = useUserStore();
+// 类型
+import { FormInstance, FormRules } from "element-plus";
+import { LocationQuery, LocationQueryValue, useRoute } from "vue-router";
 
+const userStore = useUserStore();
+const route = useRoute();
+
+const loginFormRef = ref<FormInstance>();
 const loginData = ref<LoginData>({
   username: "admin",
   password: "111111",
 });
-
+const loginRules = ref<FormRules>({
+  username: [
+    {
+      required: true,
+      message: "请输入用户名",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "请输入密码",
+      trigger: "blur",
+    },
+  ],
+});
 function handleLogin() {
-  userStore.login(loginData.value).then(() => userStore.getInfo());
+  loginFormRef.value?.validate((valid) => {
+    if (valid) {
+      userStore.login(loginData.value).then(() => {
+        const query: LocationQuery = route.query;
+        const redirect = (query.redirect as LocationQueryValue) ?? "/";
+
+        const queryOtherParams = Object.keys(query).reduce((acc, key) => {
+          if (key !== "redirect") {
+            acc[key] = query[key];
+          }
+          return acc;
+        }, {} as LocationQuery);
+
+        router.push({ path: redirect, query: queryOtherParams });
+      });
+    }
+  });
 }
 </script>
 
 <template>
   <div class="login-container">
     <div class="login-form">
-      <el-form ref="loginForm" :model="loginData">
-        <el-form-item>
+      <el-form ref="loginFormRef" :model="loginData" :rules="loginRules">
+        <el-form-item prop="username">
           <el-input v-model="loginData.username" :prefix-icon="UserFilled" />
         </el-form-item>
 
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="loginData.password" :prefix-icon="Lock" show-password />
         </el-form-item>
       </el-form>
